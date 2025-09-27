04c2279f00156d0e7497843c562b824f06113f25
Tim Newsham <tim@fly.io>
2025-09-27 12:27:11 -1000

    cleanup and goofing

--- app/Main.hs
+++ app/Main.hs
@@ -10,31 +10,43 @@ import Graphics.Gloss (Color, makeColor, white, black)
 
 -- returns a color for each (x,y,ts) with (x,y) normalized to 0..1 and ts in seconds.
 blinkingGradient :: Float -> Float -> Float -> Color
-blinkingGradient x y ts = makeColor x y (mod' (ts/2) 1.0) 1
+--blinkingGradient x y ts = makeColor x y (mod' (ts/2) 1.0) 1
+blinkingGradient x y ts = lerp grad white blink
+  where
+    grad = makeColor x y 0 1
+    blink = mod' (ts/2) 1.0
 
-getCirc :: Float -> Float -> Float -> Color
-getCirc x y ts = if r < 0.75 then (blinkingGradient x y ts) else black
+circle :: Float -> Float -> Float -> Color
+circle x y ts = if r < 0.75 then (blinkingGradient x y ts) else black
   where
     xx = lerp (-1.0) 1.0 x
     yy = lerp (-1.0) 1.0 y
-    r = sqrt (xx*xx + yy*yy)
-
--- lerp returns the linear interpolation between minv and maxv based on t, which is clamped to [0..1].
--- TODO: generalize this. It should work for any scalable value for float t.
---lerp :: Scale a => a -> a -> Float -> a
-lerp :: Float -> Float -> Float -> Float
-lerp minv maxv t = scale minv (1-t') + scale maxv t'
-  where t' = clamp 0.0 1.0 t
+    r = sqrt (xx*xx + yy*yy) :: Float
 
 -- clamp return x clamped between minx an maxx.
 clamp :: Ord a => a -> a -> a -> a
 clamp minx maxx x = if x < minx then minx else if x > maxx then maxx else x
 
+-- lerp returns the linear interpolation between minv and maxv based on t, which is clamped to [0..1].
+-- You can lerp anything that can be scaled and added.
+lerp :: (Scale a, Num a) => a -> a -> Float -> a
+lerp minv maxv t = scale (1-t') minv + scale t' maxv
+  where t' = clamp 0.0 1.0 t
+
+-- Scale a is a type that can be scaled by a float.
 class Scale a where
   scale :: Float -> a -> a
 
 instance Scale Float where
   scale s x = s * x
+
+instance Scale Int where
+  scale s x = floor (s * fromIntegral x)
+
+-- Scaling a color adjusts the RGB channels but leaves A unchanged.
+instance Scale Color where
+  scale s c = makeColor (s*r) (s*g) (s*b) a
+    where (r,g,b,a) = G.rgbaOfColor c
 
 -- genRGBA returns an RGBA bitmap with size (xsz,ysz) using picfunc.
 genRGBA :: (Int, Int) -> (Float -> Float -> Float -> Color) -> Float -> G.Picture
@@ -51,8 +63,8 @@ genRGBA (xsz, ysz) picfunc ts = G.bitmapOfByteString xsz ysz fmt bs False
 
 main :: IO ()
 main = do
-  --let f = getColor
-  let f = getCirc
+  --let f = blinkingGradient
+  let f = circle
   let wsz = (500, 500)
   let disp = G.InWindow "Animate!" wsz (10, 10)
   G.animate disp white (genRGBA wsz f)
