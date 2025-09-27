5f33fe50b6954b135d4078a25dd9bac8242360e0
Tim Newsham <tim@fly.io>
2025-09-27 13:56:19 -1000

    work on types

--- app/GraphLib.hs
+++ app/GraphLib.hs
@@ -1,6 +1,10 @@
 module GraphLib (
   display
+  , Image
+  , ColorImage
+  , Bitmap
   , Anim
+  , ColorAnim
 
   -- re-exports.
   , Color
@@ -14,13 +18,30 @@ import qualified Data.ByteString as B
 import qualified Graphics.Gloss as G
 import Graphics.Gloss (Color, makeColor, white, black)
 
-type Anim = Float -> Float -> Float -> Color
+-- An image maps (x,y) in range [0..1] to values.
+type Image a = Float -> Float -> a
+
+-- A bitmap is an image of Bools.
+-- It maps (x, y) in range [0..1] to Bools.
+type Bitmap = Image Bool
+
+-- A ColorImage is an image of Colors.
+-- It maps (x, y) in range [0..1] to Colors.
+type ColorImage = Image Color
+
+-- An Anim maps timestamps in seconds to images.
+-- It maps timestamps in seconds and (x,y) in range [0..1] to values.
+type Anim a = Float -> Image a
+
+-- A ColorAnim is animation of Color images.
+-- It maps timestamps in seconds and (x,y) in range [0..1] to Colors.
+type ColorAnim = Anim Color
 
 -- genRGBA returns an RGBA bitmap with size (xsz,ysz) using picfunc.
-genRGBA :: (Int, Int) -> Anim -> Float -> G.Picture
+genRGBA :: (Int, Int) -> ColorAnim -> Float -> G.Picture
 genRGBA (xsz, ysz) picfunc ts = G.bitmapOfByteString xsz ysz fmt bs False
   where
-    bs = B.pack $ concat $ [color2bytes (picfunc (frac x xsz) (frac y ysz) ts) | y <- [0..ysz-1], x <- [0..xsz-1]]
+    bs = B.pack $ concat $ [color2bytes (picfunc ts (frac x xsz) (frac y ysz)) | y <- [0..ysz-1], x <- [0..xsz-1]]
     clampToByte x = fromIntegral (floor (x * 255) :: Int)
     color2bytes :: Color -> [Word8]
     color2bytes c = let (r,g,b,a) = G.rgbaOfColor c in map clampToByte [r,g,b,a]
@@ -30,7 +51,7 @@ genRGBA (xsz, ysz) picfunc ts = G.bitmapOfByteString xsz ysz fmt bs False
     fmt = G.BitmapFormat G.BottomToTop G.PxRGBA
 
 -- display will show an animation defined by f in a window
-display :: Anim -> IO ()
+display :: ColorAnim -> IO ()
 display f = do
   let wsz = (500, 500)
   let disp = G.InWindow "Animate!" wsz (10, 10)
--- app/Main.hs
+++ app/Main.hs
@@ -7,13 +7,16 @@ import Lib
 main :: IO ()
 main = display circle
 
--- returns a color for each (x,y,ts) with (x,y) normalized to 0..1 and ts in seconds.
-blinkingGradient :: Float -> Float -> Float -> Color
-blinkingGradient x y ts = lerp grad white (cosCycle 2 ts)
+
+-- returns a color for each (ts, x,y) with (x,y) normalized to 0..1 and ts in seconds.
+blinkingGradient :: ColorAnim
+-- blinkingGradient :: Float -> Float -> Float -> Color
+blinkingGradient ts x y = lerp grad white (cosCycle 2 ts)
   where grad = makeColor x y 0 1
 
-circle :: Float -> Float -> Float -> Color
-circle x y ts = if r < 0.75 then (blinkingGradient x y ts) else black
+circle :: ColorAnim
+-- circle :: Float -> Float -> Float -> Color
+circle ts x y = if r < 0.75 then (blinkingGradient ts x y) else black
   where
     xx = lerp (-1.0) 1.0 x
     yy = lerp (-1.0) 1.0 y
