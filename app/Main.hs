82818aaae5ae14626a95e2bb8274aa54996fbc7e
Tim Newsham <tim@fly.io>
2025-09-27 16:13:19 -1000

    throb circle

--- app/Main.hs
+++ app/Main.hs
@@ -6,11 +6,12 @@ import Lib
 import Types
 
 main :: IO ()
-main = animOrigin circle
--- main = animOrigin (mapAnim (scaleImage 0.5) circle)
--- main = animOrigin (mapAnim (translateImage 0.3 0 . scaleImage 0.5) circle)
--- main = animOrigin (translateAnim 10 (scaleAnim 0.5 circle))
--- main = animOrigin (scaleAnim 0.5 circle)
+-- main = animOrigin circle075
+main = animOrigin throbCircle
+-- main = animOrigin (mapAnim (scaleImage 0.5) circle075)
+-- main = animOrigin (mapAnim (translateImage 0.3 0 . scaleImage 0.5) circle075)
+-- main = animOrigin (translateAnim 10 (scaleAnim 0.5 circle075))
+-- main = animOrigin (scaleAnim 0.5 circle075)
 -- main = animOrigin (constAnim (bwBitmap (bmCircle 0.75)))
 
 -- a gradient centered at the origin that cycles with time.
@@ -22,8 +23,14 @@ blinkingGradient ts x y = lerp grad white (cosCycle 2 ts)
     y' = unlerp (-1.0) 1.0 y
 
 -- a circle at the origin colored by a blinking grandient.
-circle :: ColorAnim
-circle ts = maskImage (constImage black) (blinkingGradient ts) (bmCircle 0.75)
+circle :: Float -> ColorAnim
+circle rad ts = maskImage (constImage black) (blinkingGradient ts) (bmCircle rad)
+
+throbCircle :: ColorAnim
+throbCircle ts = circle (0.1 + 0.75 * (cosCycle 7 ts)) ts
+
+circle075 :: ColorAnim
+circle075 = circle 0.75
 
 -- a circle at the origin with radius rad.
 bmCircle :: Float -> Bitmap
